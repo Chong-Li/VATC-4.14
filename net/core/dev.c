ab7a959de1108e407db1dd2b5685294558a31930
Chong-Li <lichong659@gmail.com>
2018-12-05 21:38:05 -0600

    debug

--- net/core/dev.c
+++ net/core/dev.c
@@ -3630,16 +3630,18 @@ static inline void ____napi_schedule(struct softnet_data *sd,
 				     struct napi_struct *napi)
 {
 	/*VATC*/
-	if (!memcmp(napi->dev->name, "ens1", 4)){
-		//printk("~~~~~~~~~~~~~~~ napi_sched: %s\n",napi->dev->name);
-		if (list_empty(&napi->kthread_list)){
-			list_add_tail(&napi->kthread_list, &sd->kthread_list);
-			net_recv_flag = 1;
-			if(!list_empty(&(net_recv_wq.head))){
-				wake_up(&net_recv_wq);
+	if (napi->dev != NULL){
+		if (!memcmp(napi->dev->name, "ens1", 4)){
+			//printk("~~~~~~~~~~~~~~~ napi_sched: %s\n",napi->dev->name);
+			if (list_empty(&napi->kthread_list)){
+				list_add_tail(&napi->kthread_list, &sd->kthread_list);
+				net_recv_flag = 1;
+				if(!list_empty(&(net_recv_wq.head))){
+					wake_up(&net_recv_wq);
+				}
 			}
+			return;
 		}
-		return;
 	}
 	//printk("napi_sched: %s\n",napi->dev->name);
 	list_add_tail(&napi->poll_list, &sd->poll_list);
